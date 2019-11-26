81a29b0e3bc284021a2c2ae7613927feadaadb80
penguin020 <nieland@gmail.com>
2019-11-26 15:20:49 -0500

    After cleanup

--- ConvertToModulesPOC/scripts/yMod.ts
+++ ConvertToModulesPOC/scripts/yMod.ts
@@ -1,3 +1,8 @@
 import gammaAlias from "../scripts/gamma.js";
 
+declare global {
+    export type gamma = gammaAlias;
+    export const gamma: typeof gammaAlias;
+}
+
 (window as any)["gamma"] = gammaAlias;
